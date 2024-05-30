725e1d87693f8c64bf29e8d7ebd8833fc95de368
ja1423 <aziabdujabborova1423@gmail.com>
2024-05-30 07:52:04 +0500

    begin

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<script>
+import { defineComponent } from '@vue/composition-api'
+import { mapGetters } from "vuex";
+import Slide from "./Slide.vue";
+
+
+
+export default mapGetters({
+    setup() {
+        
+    },
+})
+</script>
